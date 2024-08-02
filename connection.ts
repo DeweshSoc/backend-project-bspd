3ed8dcb1b67ef1434932bb91e09aace6384b76cc
Dewesh Jha <deweshjha4@gmail.com>
2024-08-02 23:31:12 +0530

    add db name to connection

--- connection.ts
+++ connection.ts
@@ -1,14 +1,15 @@
 import { Sequelize } from "sequelize";
 import { ErrorResponse } from "./src/interfaces";
 
+const dbName = process.env.DB_NAME || null;
 const host = process.env.DB_ENDPOINT || null;
 const username = process.env.DB_USERNAME || null;
 const password = process.env.DB_PASSWORD || null;
 const port = Number(process.env.DB_PORT) || 3306;
 const dialect = "mysql";
 
-if (!host || !username || !password) {
-    console.table({ host, username, password });
+if (!dbName || !host || !username || !password) {
+    console.table({ dbName, host, username, password });
     const err = Error(
         "connection attempt failed: missing parameters"
     ) as ErrorResponse;
@@ -16,7 +17,7 @@ if (!host || !username || !password) {
     throw err;
 }
 
-const connection = new Sequelize("", username, password, {
+const connection = new Sequelize(dbName, username, password, {
     host,
     port,
     dialect,
