1c8fe85ef5f3393cc4add4bbe0066c5bf0c3a592
Dewesh Jha <deweshjha4@gmail.com>
2024-08-02 14:23:40 +0530

    configure for cors

--- app.ts
+++ app.ts
@@ -11,6 +11,17 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "src", "public")));
 
+// cors
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    next();
+});
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
