52390836c40acc486cf259faef05c88595ae8c5e
Dewesh Jha <deweshjha4@gmail.com>
2024-08-02 14:22:52 +0530

    configure express app

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
+import * as path from "path";
+
 import dotenv from "dotenv";
-import express from "express";
+import express,{Request,Response,NextFunction} from "express";
 
 dotenv.config();
 
 const app = express();
+
+// configurations
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "src", "public")));
 
 
 let port = process.env.PORT;
