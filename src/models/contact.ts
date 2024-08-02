1db4649745034231f13d51cf858e6c8b77406062
Dewesh Jha <deweshjha4@gmail.com>
2024-08-02 16:50:53 +0530

    add Contact model

---
+++ src/models/contact.ts
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import connection from "../../connection";
+
+const Contact = connection.define(
+    "Contact",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        email: DataTypes.STRING,
+
+        phoneNumber: DataTypes.STRING,
+
+        linkedId:  DataTypes.INTEGER,
+
+        linkPrecedence: {
+            type: DataTypes.ENUM("primary", "secondary"),
+            allowNull: false,
+        },
+
+    },
+    {
+        timestamps: true, // for createdAt, updatedAt
+        paranoid: true, // Calling destroy will not delete the model, but instead set a deletedAt timestamp 
+    }
+);
+
+export default Contact;
